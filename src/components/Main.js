72e47c9995b5c199537e5c0f478da023479e893c
박상호 <a328028@gmail.com>
2022-06-27 00:03:23 +0900

    Feat: 기능 구현

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,96 +1,57 @@
-const { useState } = require("react");
-
-const api = {
-  key: "c804c2461e2d3849e26d07926609f755",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
+const { useState, useEffect } = require("react");
 
 function Main() {
-  const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({});
+  const apiKey = "c804c2461e2d3849e26d07926609f755";
+  const [datas, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
 
-  const search = (e) => {
-    if (e.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (data) => {
+        setLatitude(data.coords.latitude);
+        setLongitude(data.coords.longitude);
+      },
+      () => {
+        console.log("err");
+      }
+    );
+  };
+
+  const getData = () => {
+    setLoading(true);
+    setTimeout(() => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+      )
         .then((res) => res.json())
-        .then((result) => {
-          setWeather(result);
-          setQuery("");
-          console.log(result);
+        .then((data) => setData(data))
+        .then(() => setLoading(false))
+        .catch(() => {
+          console.log("err");
         });
-    }
+    }, 300);
   };
 
-  const dateBuilder = (d) => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
+  useEffect(() => {
+    getLocation();
+  }, []);
 
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+  useEffect(() => {
+    if (latitude !== null) getData();
+  }, [latitude]);
 
-    return `${day} ${date} ${month} ${year}`;
-  };
+  if (loading) return null;
+
+  console.log(datas.name, latitude, longitude);
+  console.log(datas.main.temp);
 
   return (
-    <div
-      className={
-        typeof weather.main != "undefined"
-          ? weather.main.temp > 16
-            ? "app warm"
-            : "app"
-          : "app"
-      }
-    >
-      <main>
-        <div className="search-box">
-          <input
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={(e) => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
-        {typeof weather.main != "undefined" ? (
-          <div>
-            <div className="location- box">
-              <div className="location">
-                {weather.name}. {weather.sys.countyu}
-              </div>
-              <div className="date">{dateBuilder(new Date())}</div>
-            </div>
-            <div className="weather-box">
-              <div className="temp">{Math.round(weather.main.temp)}°C</div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
-          </div>
-        ) : (
-          ""
-        )}
-      </main>
+    <div className="main-container">
+      <p>{datas.name}</p>
+      <p>{datas.main.temp}</p>
+      <p>{datas.weather[0].main}</p>
     </div>
   );
 }
