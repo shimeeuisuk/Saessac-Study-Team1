84ab45eb9a9e8624bac162f19032b2192da26101
0SCAR0421 <oscar0421@kakao.com>
2022-06-28 23:55:32 +0900

    Feat: add signin

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,11 +1,45 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const axios = require('axios')
 
 function Signin() {
+  const [datas, setData] = useState({})
+
+  const getUserID = (e) => {
+    setData({...datas, userid: e.target.value})
+  }
+
+  const getUserPassword = (e) => {
+    setData({...datas, userpassword: e.target.value})
+  }
+
+  const trySignin = async () => {
+    try{
+      const res = await axios.post('http://34.168.215.145/user/login', 
+      {
+        userid: datas.userid,
+        userpassword: datas.userpassword
+      })
+      const data = res.data
+
+      console.log(data.uid)
+    } catch(err){
+      console.log(err)
+    }
+  }
+
+  const pushEnter = (e) => {
+    if(e.keyCode === 13) trySignin()
+  }
+
   return(
     <div>
-      로그인
+      <p>로그인</p>
+      <p>id</p><input onChange={getUserID} onKeyDown={pushEnter} />
+      <p>password</p><input type="password" onChange={getUserPassword} onKeyDown={pushEnter} />
+      <Link to="/signup"><p>회원가입</p></Link>
+      <div onClick={trySignin}>Login</div>
     </div>
   )
 }
