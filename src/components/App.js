b48f02c05100aa10598a325299ecde3c4f2a1242
shimeuisuk <euisuk95@gmail.com>
2022-07-01 23:31:17 +0900

    Feat:경로변경

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,43 @@
-import '../css/App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Signup from './Signup';
-import Signin from './Signin';
-import Main from './Main';
-import PostList from './PostList';
-import PostDetail from './PostDetail';
-import MyPage from './MyPage';
+import "../css/App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Signup from "./Signup";
+import Signin from "./Signin";
+import Main from "./Main";
+import PostList from "./PostList";
+import PostDetail from "./PostDetail";
+import MyPage from "./MyPage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      Welcome Saessac!
-      <Routes>
-        <Route path='/' element={<Main />}></Route>
-        <Route path='/signup' element={<Signup />}></Route>
-        <Route path='/signin' element={<Signin />}></Route>
-        <Route path='/mypage' element={<MyPage />}></Route>
-        <Route path='/postlist' element={<PostList />}></Route>
-        <Route path='/postdetail' element={<PostDetail />}></Route>
-      </Routes>
-      <Link to="/"><p>메인</p></Link>
-      <Link to="/signup"><p>회원가입</p></Link>
-      <Link to="/signin"><p>로그인</p></Link>
-      <Link to="/mypage"><p>마이페이지</p></Link>
-      <Link to="/postlist"><p>글 목록</p></Link>
-      <Link to="/postdetail"><p>글 내용</p></Link>
+        Welcome Saessac!
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/mypage" element={<MyPage />}></Route>
+          <Route path="/postlist" element={<PostList />}></Route>
+          <Route path="/postdetail/:id" element={<PostDetail />}></Route>
+        </Routes>
+        <Link to="/">
+          <p>메인</p>
+        </Link>
+        <Link to="/signup">
+          <p>회원가입</p>
+        </Link>
+        <Link to="/signin">
+          <p>로그인</p>
+        </Link>
+        <Link to="/mypage">
+          <p>마이페이지</p>
+        </Link>
+        <Link to="/postlist">
+          <p>글 목록</p>
+        </Link>
+        <Link to="/postdetail">
+          <p>글 내용</p>
+        </Link>
       </BrowserRouter>
     </div>
   );
