4ca33f0b5e203acbf07be494cdd8788e7d4289c4
hopak-e <a328028@gmail.com>
2022-07-03 20:29:25 +0900

    Feat: 러닝메이트 및 러닝장소 링크 구현

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,48 +1,60 @@
+import { Link } from "react-router-dom";
+import "../css/Main.css";
 const { useState, useEffect } = require("react");
 
 function Main() {
   const apiKey = "c804c2461e2d3849e26d07926609f755";
   const [datas, setData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [location, setLocation] = useState({latitude: null, longitude: null});
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
 
   const getLocation = () => {
-    navigator.geolocation.getCurrentPosition((data)=> {
-      setLocation({latitude: data.coords.latitude, longitude: data.coords.longitude})
-    }, () => {
-      setLocation({latitude: 'err', longitude: 'err'})
-    })
-  }
+    navigator.geolocation.getCurrentPosition(
+      (data) => {
+        setLocation({
+          latitude: data.coords.latitude,
+          longitude: data.coords.longitude,
+        });
+      },
+      () => {
+        setLocation({ latitude: "err", longitude: "err" });
+      }
+    );
+  };
 
   const getNoLocationData = () => {
     setLoading(true);
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?id=1838524&appid=${apiKey}`)
-    .then(res => res.json())
-    .then(data => setData(data))
-    .then(() => setLoading(false))
-    .catch(() => {console.log('err')})
-  }
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?id=1838524&appid=${apiKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .then(() => setLoading(false))
+      .catch(() => {
+        console.log("err");
+      });
+  };
 
   const getLocationData = () => {
     setLoading(true);
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${apiKey}&units=metric`
-      )
-        .then((res) => res.json())
-        .then((data) => setData(data))
-        .then(() => setLoading(false))
-        .catch(() => {
-          console.log("err");
-        });
-    };
-
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${apiKey}&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .then(() => setLoading(false))
+      .catch(() => {
+        console.log("err");
+      });
+  };
 
   useEffect(() => {
-    getLocation()
-    if(location.latitude !== null && location.latitude !== 'err') getLocationData()
-    if(location.latitude === 'err') getNoLocationData()
-  }, [location.latitude])
+    getLocation();
+    if (location.latitude !== null && location.latitude !== "err")
+      getLocationData();
+    if (location.latitude === "err") getNoLocationData();
+  }, [location.latitude]);
 
   if (loading) return null;
 
@@ -57,10 +69,14 @@ function Main() {
         <p>{datas.weather[0].main}</p>
       </div>
       <div className="main-container__bottom">
-        <div>러닝메이트 찾기</div>
+        <button>
+          <Link to="/postlist">러닝메이트 찾기</Link>
+        </button>
+        <button>
+          <Link to="/postlist">러닝장소 추천</Link>
+        </button>
       </div>
     </div>
-
   );
 }
 
