a23e075bd0621ff588481165fb6da30ddd8e267f
hopak-e <a328028@gmail.com>
2022-07-19 00:46:03 +0900

    Feat:메인 날씨 추가

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import "../css/Weather.css";
 
 const Weather = () => {
   const apiKey = "c804c2461e2d3849e26d07926609f755";
   const [datas, setData] = useState(null);
+  const [mainData, setMainData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [location, setLocation] = useState({ latitude: null, longitude: null });
 
@@ -16,28 +18,26 @@ const Weather = () => {
         });
       },
       () => {
-        setLocation({ latitude: 35.1028, longitude: 129.0403 });
+        setLocation({ latitude: "err", longitude: "err" });
       }
     );
   };
 
-  const getNoLocationData = () => {
+  const getMainWeather = () => {
     setLoading(true);
-
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?id=1838524&appid=${apiKey}&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=${apiKey}&units=metric`
     )
       .then((res) => res.json())
       .then((data) => {
-        const newList = [
+        const mainweather = [
           {
+            icon: data.weather[0].icon.replace("n", "d"),
             temp: data.main.temp,
-            time: data.dt_txt,
           },
         ];
-        setData(newList);
+        setMainData(mainweather);
       })
-
       .then(() => setLoading(false))
       .catch(() => {
         console.log("err");
@@ -53,6 +53,7 @@ const Weather = () => {
       .then((data) => {
         const filteredList = data.list.filter((el, idx) => idx > 1 && idx < 7);
         const mappingList = filteredList.map((el) => ({
+          icon: el.weather[0].icon.replace("n", "d"),
           temp: el.main.temp,
           time: el.dt_txt,
         }));
@@ -63,22 +64,46 @@ const Weather = () => {
         console.log("err");
       });
   };
+
   useEffect(() => {
     getLocation();
-    if (location.latitude !== null && location.latitude !== "err")
+    if (location.latitude !== null && location.latitude !== "err") {
+      getMainWeather();
       getWeekWeather();
-    // if (location.latitude === "35.1028") getNoLocationData();
+    }
   }, [location.latitude]);
-  console.log(datas);
+
   return (
     <>
-      {datas &&
-        datas.map((el, idx) => (
-          <div key={idx}>
-            <span>{el.temp}</span>
-            <span>{el.time.slice(11, 13)}</span>
-          </div>
-        ))}
+      <div className="Weather-main">
+        {mainData &&
+          mainData.map((el, idx) => (
+            <div key={idx} className="">
+              <img
+                src={`http://openweathermap.org/img/wn/${el.icon}@2x.png`}
+                alt="날씨이"
+              />
+              <span>{el.temp}</span>
+            </div>
+          ))}
+      </div>
+      <div className="weather-sub">
+        {datas &&
+          datas.map((el, idx) => (
+            <span key={idx} className="">
+              <span>
+                <img
+                  src={`http://openweathermap.org/img/wn/${el.icon}@2x.png`}
+                  alt="날씨"
+                />
+              </span>
+              <span className="weather_right">
+                <span>{el.time.slice(11, 13)}시</span>
+                <span>{el.temp}</span>
+              </span>
+            </span>
+          ))}
+      </div>
     </>
   );
 };
