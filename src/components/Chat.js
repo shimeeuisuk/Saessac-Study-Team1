a57e1d0f34b9d64615ac8fe79de15f514322e849
hopak-e <a328028@gmail.com>
2022-07-20 22:20:23 +0900

    Feat:댓글 기능

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,51 +1,77 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { getLoginCookie } from "../lib/cookie";
 
-const Chat = () => {
-  const [comment, setComment] = useState("");
-  const [commentArray, setCommentArray] = useState([]);
+export const ChatContainer = styled.div`
+  background: tomato;
+`;
 
-  const onChange = (event) => setComment(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (comment === "") {
-      return;
-    }
-    setCommentArray((commentValueList) => [comment, ...commentValueList]);
-    setComment("");
+export const ChatList = styled.div`
+  background: skyblue;
+`;
+
+export const ChatInput = styled.input`
+  background: tomato;
+`;
+
+const Chat = ({ tid }) => {
+  const [comment, setComment] = useState([]);
+  const [loading, setLoading] = useState(true);
+  console.log(tid);
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  const getComments = () => {
+    axios
+      .get(`http://34.168.215.145/topiccomments/${tid}`, {
+        headers: { Authorization: getLoginCookie() },
+      })
+      .then((res) => {
+        setComment(res.data);
+        setLoading(false);
+        console.log(res.data);
+      });
   };
 
+  const handleInput = (e) => {
+    const data = {
+      topic_tid: e.tid,
+      topiccomment: e.target.value,
+    };
+    if (e.key === "Enter") {
+      console.log("dd");
+      axios
+        .post(`http://34.168.215.145/topiccomments/insert`, data, {
+          headers: { Authorization: getLoginCookie() },
+        })
+        .then((res) => {
+          getComments();
+          res.target.value = "";
+        });
+    }
+  };
+  if (loading) return null;
   return (
-    <>
-      <div className="chat-container" onSubmit={onSubmit}>
-        <form className="chat-form">
-          <input
-            type="text"
-            placeholder="소중한 의견을 나눠보아요!"
-            value={comment}
-            onChange={onChange}
-          />
-          <button type="submit">등록</button>
-        </form>
-      </div>
-      <div>
-        <ul className="comment">
-          {commentArray.map((value, index) => (
-            <li key={index} className="commentText">
-              <div className="commentMargin">
-                <span className="commnetNameBold"></span>
-                {value}
-              </div>
-              <div className="commentStart">
-                <i className="far fa-trash-alt" />
-                <i className="fas fa-heart colorHeart" />
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <ChatContainer>
+      <ChatInput onChange={handleInput} onKeyUp={handleInput} />
+      <ChatList>
+        {comment.map((e, i) => (
+          <div key={i}>
+            {e.nickname}: {e.topicComent}
+          </div>
+        ))}
+      </ChatList>
+    </ChatContainer>
   );
 };
 
 export default Chat;
+
+// console.log(props.detail.tid);
+{
+  /* <section>
+<Chat tid={props.detail.tid}></Chat>
+</section> */
+}
