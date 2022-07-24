6a5bfa92bb72cefb2c57db391309e37a5abc0663
moondoyeon <mmmdo21@gmail.com>
2022-07-24 17:21:48 +0900

    Feat: 내가쓴글 보기, 닫기 버튼 추가

--- src/components/MyPosts.js
+++ src/components/MyPosts.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/MyPage.css';
 
 export default function MyPost ({ myPosts }){
 
-return(
+const [isClicked, setIsClicked] = useState(false);
 
+const clickHandler = () => {
+  setIsClicked(!isClicked);
+}
+return(
   <div className="mypage-bottom__wrapper">
     <div className="mypage-bottomleft__title">
       <h3>내가 쓴 글보기</h3>
     </div>
+    
     <div className="mypage-bottomright__content">
+      <div className='mypage-bottomright__myPostsClickBtnContainer'>
+        <button className='mypage-btnUnderline' onClick={clickHandler}>{isClicked ? null : `보기`}</button>
+      </div>
+    { isClicked ? (
+      <div className='mypage-bottomright__myPostsContainer'>
       {myPosts.map(post => (
-        <li key={post.tid}>
-          <a href={`/postdetail/${post.tid}`}><span>{post.topicTitle}</span></a>
+        <li className='mypage-bottomright__myPostsWrapper' key={post.tid}>
+          <a className='mypage-bottomright__myPost' href={`/postdetail/${post.tid}`}>
+            <span>▶︎ </span>
+            <span className='mypage-bottomright__myPostTitle'>{post.topicTitle}</span>
+          </a>
         </li>
       ))}
+      <div className='mypage-bottomright__myPostsCloseBtnWrapper'>
+        <button className='mypage-btnUnderline' onClick={clickHandler}>닫기</button>
+      </div>
+      </div>
+    ) : null }
     </div>
   </div>  
 );
